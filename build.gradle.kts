6b1eeec1f61d120d91f6333a0093373abc20f710
blacklisting <who@knows.me>
2023-08-11 01:57:11 +0800

    Add local publishing repository.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.21"
+    `maven-publish`
 }
 
 group = "com.blacklisting"
@@ -11,4 +12,33 @@ repositories {
 
 dependencies {
     implementation("com.opencsv", "opencsv", "5.8")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "LocalM2"
+        }
+    }
+    publications {
+        create<MavenPublication>("blacklisting.lib") {
+            groupId = "com.blacklisting"
+            artifactId = "lib"
+            version = "0.0.0.1"
+            from(components["java"])
+            pom {
+                name.set("blacklisting.lib")
+                developers {
+                    developer {
+                        id.set("Blacklisting")
+                        name.set("Blacklisting")
+                        email.set("who@knows.me")
+                    }
+                }
+                scm {
+                    url.set("git@github.com:blacklisting/lib")
+                }
+            }
+        }
+    }
 }
